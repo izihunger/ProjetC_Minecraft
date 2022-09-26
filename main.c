c992589498f61afced210150c79b6cf7264b67e9
Axel HEGO <84026645+izihunger@users.noreply.github.com>
2022-09-26 20:16:24 +0200

    allocationDynamiqueMap

--- main.c
+++ main.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Bloc
+typedef struct Bloc
 {
-    char * display;
+    char display;
     int crossable;
     int drop;
-};
+}Bloc;
 
+void generateMap(Bloc ** map){
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; i < 5; i++){
+            map[i][j].display = 'd';
+        }
+    }
+}
+
+void afficherMap(Bloc ** map){
+    for(int i = 0; i < sizeof(map); i++){
+        for(int j = 0; i < 5; i++){
+            printf("%c", map[i][j].display);
+        }
+    }
+}
 
 int main(){
-    char c;
-    while(1){
-        c = getchar();
-        printf("%c", c);
+    Bloc ** map = NULL;
+    map = (Bloc**) malloc(5 * sizeof(Bloc*));
+    for (int i = 0; i < 5; i++)
+    {
+        map[i] = (Bloc*) malloc(5 * sizeof(Bloc));
     }
+    generateMap(map);
+    afficherMap(map);
     return 0;
 }
