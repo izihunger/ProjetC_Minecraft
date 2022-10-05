2f624586ca7ccac737c8455985a87793da57fec2
CR0ZER <huillet@et.esiea.fr>
2022-10-05 15:50:35 +0200

    moveMob + agro + random mvt fini

--- mob.c
+++ mob.c
@@ -14,7 +14,7 @@ void spawnMob(Bloc ** map, int size){
 }
 
 void moveMob(Bloc ** map, Player player, int size){
-    if(abs(mob.posX - player.posX) < size/5 && abs(mob.posY - player.posY) < size/5){
+    if(abs(mob.posX - player.posX) < 20 && abs(mob.posY - player.posY) < 20){
         if(abs(player.posX - mob.posX) > abs(player.posY - mob.posY)){
             if(player.posX > mob.posX){
                 if(map[mob.posY][mob.posX + 1].crossable){
@@ -22,12 +22,18 @@ void moveMob(Bloc ** map, Player player, int size){
                     mob.posX ++;
                     map[mob.posY][mob.posX].mobOn = 1;
                 }
+                else if(map[mob.posY][mob.posX + 1].playerOn){
+                    player.hp -= mob.atk;
+                }
             }
             else{
                 if(map[mob.posY][mob.posX - 1].crossable){
                     map[mob.posY][mob.posX].mobOn = 0;
                     mob.posX --;
                     map[mob.posY][mob.posX].mobOn = 1;
+                }
+                else if(map[mob.posY][mob.posX - 1].playerOn){
+                    player.hp -= mob.atk;
                 }
             }
         }
@@ -38,6 +44,9 @@ void moveMob(Bloc ** map, Player player, int size){
                     mob.posY ++;
                     map[mob.posY][mob.posX].mobOn = 1;
                 }
+                else if(map[mob.posY + 1][mob.posX].playerOn){
+                    player.hp -= mob.atk;
+                }
             }
             else{
                 if(map[mob.posY - 1][mob.posX].crossable){
@@ -45,7 +54,47 @@ void moveMob(Bloc ** map, Player player, int size){
                     mob.posY --;
                     map[mob.posY][mob.posX].mobOn = 1;
                 }
+                else if(map[mob.posY - 1][mob.posX].playerOn){
+                    player.hp -= mob.atk;
+                }
             }
+        }
+    }
+    else{
+        srand(time(NULL));
+        int a = rand() % 3;
+        switch (a)
+        {
+        case 0:
+            if(map[mob.posY - 1][mob.posX].crossable){
+                map[mob.posY][mob.posX].mobOn = 0;
+                mob.posY --;
+                map[mob.posY][mob.posX].mobOn = 1;
+            }
+            break;
+        case 1:
+            if(map[mob.posY + 1][mob.posX].crossable){
+                map[mob.posY][mob.posX].mobOn = 0;
+                mob.posY ++;
+                map[mob.posY][mob.posX].mobOn = 1;
+            }
+            break;
+        case 2:
+            if(map[mob.posY][mob.posX - 1].crossable){
+                map[mob.posY][mob.posX].mobOn = 0;
+                mob.posX --;
+                map[mob.posY][mob.posX].mobOn = 1;
+            }
+            break;
+        case 3:
+            if(map[mob.posY][mob.posX + 1].crossable){
+                map[mob.posY][mob.posX].mobOn = 0;
+                mob.posX ++;
+                map[mob.posY][mob.posX].mobOn = 1;
+            }
+            break;
+        default:
+            break;
         }
     }
 }
